7acf072d56959a6a219d393cc254300b44095a86
Aleksand Kiselev <leksandr777kiselev@yandex.ru>
2026-05-24 17:00:29 +0300

    Converting to classes. Remove sleep.

--- Tests/test_login_logout.py
+++ Tests/test_login_logout.py
@@ -4,40 +4,42 @@ from locators import *
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 
-def test_login_existing_user(driver):
-    wait = WebDriverWait(driver, 10)
-    driver.get(MAIN_URL)
+class TestLoginLogout:
 
-    wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
+    def test_login_existing_user(self,driver):
+        wait = WebDriverWait(driver, 10)
+        driver.get(MAIN_URL)
 
-    email_field = wait.until(EC.presence_of_element_located(EMAIL_FIELD))
-    email_field.clear()
-    email_field.send_keys(LOGIN_TEST_USER)
+        wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
 
-    password_field = driver.find_element(*PASSWORD_FIELD)
-    password_field.clear()
-    password_field.send_keys(PASSWORD_TEST_USER)
+        email_field = wait.until(EC.presence_of_element_located(EMAIL_FIELD))
+        email_field.clear()
+        email_field.send_keys(LOGIN_TEST_USER)
 
-    wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
-    wait.until(EC.url_contains("login"))
+        password_field = driver.find_element(*PASSWORD_FIELD)
+        password_field.clear()
+        password_field.send_keys(PASSWORD_TEST_USER)
 
-    assert wait.until(EC.visibility_of_element_located(USER_AVATAR)), "Аватар пользователя не найден"
+        wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
+        wait.until(EC.url_contains("login"))
 
-    user_name_element = driver.find_element(*USER_NAME)
-    assert user_name_element.text == "User.", f"User. не найдено"
+        assert wait.until(EC.visibility_of_element_located(USER_AVATAR)), "Аватар пользователя не найден"
+
+        user_name_element = driver.find_element(*USER_NAME)
+        assert user_name_element.text == "User.", f"User. не найдено"
 
 
-def test_logout_existing_user(driver):
-    #логин созданного пользователя
-    wait = WebDriverWait(driver, 10)
-    driver.get(MAIN_URL)
-    wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
-    wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(*LOGIN_TEST_USER)
-    driver.find_element(*PASSWORD_FIELD).send_keys(*PASSWORD_TEST_USER)
-    wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
-    wait.until(EC.url_contains("login"))
+    def test_logout_existing_user(self,driver):
+        #логин созданного пользователя
+        wait = WebDriverWait(driver, 10)
+        driver.get(MAIN_URL)
+        wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
+        wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(*LOGIN_TEST_USER)
+        driver.find_element(*PASSWORD_FIELD).send_keys(*PASSWORD_TEST_USER)
+        wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
+        wait.until(EC.url_contains("login"))
 
-    wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON))
-    wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON)).click()
+        wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON))
+        wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON)).click()
 
-    assert  wait.until(EC.visibility_of_element_located(LOGIN_REG_BUTTON)), "Аватар пользователя все еще отображается"
+        assert  wait.until(EC.visibility_of_element_located(LOGIN_REG_BUTTON)), "Аватар пользователя все еще отображается"
--- Tests/test_registrations.py
+++ Tests/test_registrations.py
@@ -4,65 +4,67 @@ from locators import *
 from helpers import generate_email
 from selenium.webdriver.support.ui import WebDriverWait
 
-def test_valid_registration(driver):
-    wait = WebDriverWait(driver, 10)
-    driver.get(MAIN_URL)
-    wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
-    wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click()
+class TestRegistration:
 
-    email = generate_email()
-    wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(email)
-    driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
-    driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
+    def test_valid_registration(self,driver):
+        wait = WebDriverWait(driver, 10)
+        driver.get(MAIN_URL)
+        wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
+        wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click()
 
-    driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
+        email = generate_email()
+        wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(email)
+        driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
+        driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
 
-    wait.until(EC.url_contains("regiatration"))
-    assert wait.until(EC.visibility_of_element_located(USER_AVATAR)), "Аватар пользователя не найден"
+        driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
 
-    user_name_element = driver.find_element(*USER_NAME)
-    assert user_name_element.text == "User.", f"User. не найдено"
+        wait.until(EC.url_contains("regiatration"))
+        assert wait.until(EC.visibility_of_element_located(USER_AVATAR)), "Аватар пользователя не найден"
 
-def test_invalid_email_format(driver):
-    wait = WebDriverWait(driver, 10)
-    driver.get(MAIN_URL)
-    wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
-    wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click()
+        user_name_element = driver.find_element(*USER_NAME)
+        assert user_name_element.text == "User.", f"User. не найдено"
 
-    wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys("bad_email_mail.ru")
-    driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
-    driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
-    driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
+    def test_invalid_email_format(self, driver):
+        wait = WebDriverWait(driver, 10)
+        driver.get(MAIN_URL)
+        wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
+        wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click()
 
-    assert  wait.until(EC.visibility_of_element_located(MYSTAKE_MESSAGE)), "Ошибка не отобразилась"
+        wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys("bad_email_mail.ru")
+        driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
+        driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
+        driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
 
-    mystakes_border = driver.find_elements(*MYSTAKE_BORDERS)
-    assert  len(mystakes_border)>=3, "Не все поля ввода не подсвечены красным"
+        assert  wait.until(EC.visibility_of_element_located(MYSTAKE_MESSAGE)), "Ошибка не отобразилась"
 
-def test_register_existing_user(driver):
-    wait = WebDriverWait(driver, 10)
-    driver.get(MAIN_URL)
-    wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
-    wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click() 
+        mystakes_border = driver.find_elements(*MYSTAKE_BORDERS)
+        assert  len(mystakes_border)>=3, "Не все поля ввода не подсвечены красным"
 
-    #регистрация тестового юзера первичная
-    first_email = generate_email()
-    wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(first_email)
-    driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
-    driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
-    driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
-    wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON)).click()  
+    def test_register_existing_user(self, driver):
+        wait = WebDriverWait(driver, 10)
+        driver.get(MAIN_URL)
+        wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
+        wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click() 
 
-    #регистрация тестового юзера повторная
-    wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
-    wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click() 
-    wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(first_email)
-    driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
-    driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
-    driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
+        #регистрация тестового юзера первичная
+        first_email = generate_email()
+        wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(first_email)
+        driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
+        driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
+        driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
+        wait.until(EC.element_to_be_clickable(LOGOUT_BUTTON)).click()  
 
-    assert  wait.until(EC.visibility_of_element_located(MYSTAKE_MESSAGE)), "Ошибка не отобразилась"
+        #регистрация тестового юзера повторная
+        wait.until(EC.element_to_be_clickable(LOGIN_REG_BUTTON)).click()
+        wait.until(EC.element_to_be_clickable(NO_ACCOUNT_BUTTON)).click() 
+        wait.until(EC.presence_of_element_located(EMAIL_FIELD)).send_keys(first_email)
+        driver.find_element(*PASSWORD_FIELD).send_keys("qwerty123")
+        driver.find_element(*CONFIRM_PASSWORD_FIELD).send_keys("qwerty123")
+        driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
 
-    mystakes_border = driver.find_elements(*MYSTAKE_BORDERS)
-    assert  len(mystakes_border)>=3, "Не все поля ввода не подсвечены красным"
+        assert  wait.until(EC.visibility_of_element_located(MYSTAKE_MESSAGE)), "Ошибка не отобразилась"
+
+        mystakes_border = driver.find_elements(*MYSTAKE_BORDERS)
+        assert  len(mystakes_border)>=3, "Не все поля ввода не подсвечены красным"
 
